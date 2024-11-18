bef2693b5e4a8a2acc6f1e4cc46913b49c651708
EmericSauthier <emeric.sauthier@etu.univ-smb.fr>
2024-11-18 16:14:27 +0100

    Partie 9 Ajout de l'extension à la fin du fichier

--- Document App/Document App/DocumentTableViewController.swift
+++ Document App/Document App/DocumentTableViewController.swift
@@ -6,17 +6,17 @@
 //
 
 import UIKit
+import QuickLook
 
 class DocumentTableViewController: UITableViewController {
     
+    var fileList: [DocumentFile]?
+    var previewController: QLPreviewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        fileList = listFileInBundle()
     }
 
     // MARK: - Table view data source
@@ -28,7 +28,7 @@ class DocumentTableViewController: UITableViewController {
 
     // Indique au Controller combien de cellules il doit afficher
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listFileInBundle().count
+        return fileList!.count
         // return DocumentFile.documents.count
     }
     
@@ -36,7 +36,7 @@ class DocumentTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
         
-        let document = listFileInBundle()[indexPath.row]
+        let document = fileList![indexPath.row]
         cell.textLabel?.text = "\(document.title)"
         cell.detailTextLabel?.text = "\(document.size.formattedSize())"
         
@@ -85,7 +85,7 @@ class DocumentTableViewController: UITableViewController {
         guard let index = tableView.indexPathForSelectedRow else { return }
         
         // 2. Récuperer le document correspondant à l'index
-        let document = listFileInBundle()[index.row]
+        let document = fileList![index.row]
         
         // 3. Cibler l'instance de DocumentViewController via le segue.destination
         // 4. Caster le segue.destination en DocumentViewController
@@ -95,62 +95,6 @@ class DocumentTableViewController: UITableViewController {
             target.imageName = document.imageName
         }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 struct DocumentFile {
@@ -181,5 +125,19 @@ extension Int {
         formatter.countStyle = .file
         
         return formatter.string(fromByteCount: Int64(self))
+    }
+}
+
+// Partie 9
+extension DocumentTableViewController {
+    // On utilise plus un segue, nous devons donc utiliser le navigationController pour afficher le QLPreviewController
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let file = fileList![indexPath.row]
+        
+        self.instantiateQLPreviewController(withUrl: file.url)
+    }
+
+    func instantiateQLPreviewController(withUrl url: URL) {
+        self.previewController = QLPreviewController(nibName: url.absoluteString, bundle: Bundle.main)
     }
 }
